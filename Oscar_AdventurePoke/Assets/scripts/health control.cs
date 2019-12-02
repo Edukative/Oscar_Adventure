5c0369bb27cdbc4ca8a31587ef418306541dd3eb
Edukative <56083723+Edukative@users.noreply.github.com>
2019-12-02 15:13:31 +0100

    UPDATE HEALTH

--- Oscar_AdventurePoke/Assets/scripts/health control.cs
+++ Oscar_AdventurePoke/Assets/scripts/health control.cs
@@ -8,6 +8,12 @@ public class healthcontrol : MonoBehaviour
     {
         Debug.Log("object that entred the trigger: " + other);
 
-        PlayerController controller = other.GetComponent<PlayerController>();//
+        PlayerController controller = other.GetComponent<PlayerController>();//get the player controller from the other thing collided with the trigger
+        if (controller != null)//if the controller retrived is not empty
+        {
+            // ! the exclamation is a negation value
+            controller.CangeHealth(1); // call the health funcion and add 1 to the health of the player
+            Destroy(gameObject); // Destroys all the game object and this script too!
+        }
     }
 }
