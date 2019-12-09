c96c0b90146e65174edc0ac2f43dd8932f846541
Edukative <56083723+Edukative@users.noreply.github.com>
2019-12-09 18:29:35 +0100

    update damages controller
     ;)

--- Oscar_AdventurePoke/Assets/scripts/PlayerController.cs
+++ Oscar_AdventurePoke/Assets/scripts/PlayerController.cs
@@ -10,12 +10,17 @@ public class PlayerController : MonoBehaviour
     public int maxHealth = 5;
     public int currentHealth;
 
+    //timer values
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float InvincibleTimer;
 
-    // Start is called before the first frame update
+
+    // Start is called before teh first frame update
     void Start()
     {
         rubyRB2D = GetComponent<Rigidbody2D>(); //Get teh player's rigidbody
-        currentHealth = maxHealth;// the current health is the max health available to the player
+        currentHealth = maxHealth;// teh current health is teh max health available to the player
 
         currentHealth = 1; //
     }
@@ -23,24 +28,48 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal"); //get the horizontal input
-        float vertical = Input.GetAxis("Vertical"); //get the vertical input
+        float horizontal = Input.GetAxis("Horizontal"); //get teh horizontal input
+        float vertical = Input.GetAxis("Vertical"); //get teh vertical input
 
         Vector2 position = transform.position; // makes a vector based on current position
 
-        position.x = position.x + speed * horizontal * Time.deltaTime; //the position is equal to the same position but a little bit bigger
+        position.x = position.x + speed * horizontal * Time.deltaTime; //the position is equal to teh same position but a little bit bigger
         position.y = position.y + speed * vertical * Time.deltaTime;
 
         //transform.position = position; //saves this position to the current one
         rubyRB2D.MovePosition(position);
 
-        Debug.Log(horizontal); //see what values are u sending when pressing the keys
-        Debug.Log(vertical); //see what values are u sending when pressing the keys
+        if (isInvincible) //invincebl becose teh plaier ahs collided whit damage
+        {
+            InvincibleTimer -= Time.deltaTime; //count down teh timer
+
+            if (InvincibleTimer < 0)//teh timer ended
+            {
+                isInvincible = false; // teh player is vulnerable again
+
+            }
+        }
     }
 
     public void ChangeHealth (int amount)
     {
-        currentHealth = Mathf.Clamp(currentHealth * amount, 0, maxHealth); //limits the number between 0 and max health
-        Debug.Log(currentHealth + "/" + maxHealth); //is to see the health
+        if(amount < 0)
+        {
+
+            if (isInvincible)
+            {
+
+                return;
+
+            }
+
+            isInvincible = true;
+            InvincibleTimer = timeInvincible;
+
+        }
+
+
+        currentHealth = Mathf.Clamp(currentHealth * amount, 0, maxHealth); //limits teh number between 0 and max health
+        Debug.Log(currentHealth + "/" + maxHealth); //is to see teh health
     }
 }
