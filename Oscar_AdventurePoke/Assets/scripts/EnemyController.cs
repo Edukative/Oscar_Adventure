071f96fd3bde66499af5d3d85d4dab0a5128969f
Edukative <56083723+Edukative@users.noreply.github.com>
2019-12-16 18:30:32 +0100

    add enemy
    ;)

---
+++ Oscar_AdventurePoke/Assets/scripts/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed; //speed of enemi
+    Rigidbody2D rb2D;
+    public bool isVertical 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>(); //get teh enemy's RigidBody  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = rb2D.position; //get teh current position of teh enemy
+
+        if (isVertical)
+        {
+            position.y = position.y + Time.deltaTime * speed;
+
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed;
+        }
+            
+
+        rb2D.MovePosition(position);
+    }
+}
